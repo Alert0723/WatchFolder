5a15a1e216e4d783a24a62106933545105e1c8d1
Alert <cong.huang.work@outlook.com>
2024-03-10 15:32:06 +0800

    first commit

---
+++ WatchFolder/FolderWatcher.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WatchFolder
+{
+    internal class FolderWatcher
+    {
+        public FolderWatcher(string path = "", string filter = "*.*")
+        {
+            if (path == string.Empty)
+            {
+                path = System.IO.Directory.GetCurrentDirectory();
+            }
+
+            string selectedFolder = path;
+            Console.WriteLine($"Watching folder: {selectedFolder}");
+
+            FileSystemWatcher watcher = new FileSystemWatcher($"{selectedFolder}");
+            watcher.NotifyFilter = NotifyFilters.Attributes | NotifyFilters.CreationTime | NotifyFilters.DirectoryName | NotifyFilters.FileName | NotifyFilters.LastAccess | NotifyFilters.LastWrite | NotifyFilters.Security | NotifyFilters.Size;
+            watcher.Filter = filter;
+            watcher.IncludeSubdirectories = true;
+            watcher.EnableRaisingEvents = true;
+            watcher.Changed += OnChanged;
+            watcher.Created += OnCreated;
+            watcher.Deleted += OnDeleted;
+            watcher.Renamed += OnRenamed;
+        }
+
+        void OnChanged(object sender, FileSystemEventArgs e)
+        {
+            if (e.ChangeType != WatcherChangeTypes.Changed)
+            {
+                return;
+            }
+            Console.WriteLine($"Changed: {e.FullPath}");
+        }
+
+        void OnCreated(object sender, FileSystemEventArgs e)
+        {
+            Console.WriteLine($"Created: {e.FullPath}");
+        }
+
+        void OnDeleted(object sender, FileSystemEventArgs e)
+        {
+            Console.WriteLine($"Deleted: {e.FullPath}");
+        }
+
+        void OnRenamed(object sender, RenamedEventArgs e)
+        {
+            Console.WriteLine($"Renamed:");
+            Console.WriteLine($"    Old: {e.OldFullPath}");
+            Console.WriteLine($"    New: {e.FullPath} ");
+        }
+    }
+}
---
+++ WatchFolder/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace WatchFolder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"args:\n1.absulute folder path.\n2.filter(for example \"*.txt\")\n");
+            new FolderWatcher();
+            Console.ReadKey();
+        }
+    }
+}
